85ebf1651194f0ff9cc436fa116b0b600bbf6970
coderGtm <gautammehta0000@gmail.com>
2022-07-30 14:12:45 +0530

    almost done now

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+function convertToTravelWorld() {
+    elem = document.getElementById("hello");
+    interval1 = setInterval(function() {
+        txt = elem.textContent;
+        if (txt!="") {
+            elem.textContent = txt.slice(0,-1);
+        }
+        else {
+            clearInterval(interval1);
+            targetString = "<Travel, World!>";
+            interval2 = setInterval(function() {
+                txt = elem.textContent;
+                if (txt!=targetString) {
+                    elem.textContent = txt+targetString[txt.length];
+                }
+                else {
+                    clearInterval(interval2);
+                }
+            },200);
+        }
+    },200);
+}
+
+setTimeout(convertToTravelWorld,3000);
